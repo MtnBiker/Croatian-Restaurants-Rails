bc6748fccf0df8d5fd7a51b45b067aefefb0eb92
MtnBiker <github@web.knobby.ws>
2015-09-23 11:08:49 -0700

    Initial Commit. See Readme for how I got to here. In a nutshell: set up a rails project and made it PostGIS. No tables yet.

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Croresrails
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+      
+    # Gauthier: Since we will be using some raw SQL, we need to switch from a schema.rb to a structure.sql. This means that in development, we're going to save our database snapshot in raw SQL, not Rails's Ruby representation.
+    config.active_record.schema_format = :sql
+end
+end
